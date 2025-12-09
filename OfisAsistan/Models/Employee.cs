6f98eb79959b120d710fc90e2c5cc7fe8fcf577c
Oğulcan Demir <ogulcandmr96@gmail.com>
2025-12-09 18:29:50 +0300

    çalışanlar gözüküyor artık

--- OfisAsistan/Models/Employee.cs
+++ OfisAsistan/Models/Employee.cs
@@ -1,20 +1,42 @@
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace OfisAsistan.Models
 {
     public class Employee
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("first_name")]
         public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
         public string LastName { get; set; }
+
+        [JsonProperty("email")]
         public string Email { get; set; }
+
+        [JsonProperty("department_id")]
         public int DepartmentId { get; set; }
+
+        [JsonProperty("position")]
         public string Position { get; set; }
+
+        [JsonProperty("skills")]
         public string Skills { get; set; } // JSON formatında yetenek listesi
+
+        [JsonProperty("current_workload")]
         public int CurrentWorkload { get; set; } // Mevcut iş yükü (saat cinsinden)
+
+        [JsonProperty("max_workload")]
         public int MaxWorkload { get; set; } // Maksimum iş yükü kapasitesi
+
+        [JsonProperty("is_active")]
         public bool IsActive { get; set; }
+
+        [JsonProperty("created_date")]
         public DateTime CreatedDate { get; set; }
         
         // Navigation properties
